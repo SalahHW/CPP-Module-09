859a67e1221cd7e1b7a9ac16664bdda6b68d2e91
SalahHW <salah.bouheniche@gmail.com>
2024-06-07 14:01:10 +0200

    Add header and includes guards

---
+++ ex01/include/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 12:42:25 by sbouheni          #+#    #+#             */
+/*   Updated: 2024/06/05 12:43:32 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange
+{
+
+};
+
+#endif
