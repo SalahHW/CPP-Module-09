78a7fc624e438465cf8e1963d8ff0bd696f06541
Salah <salah.bouheniche@gmail.com>
2024-06-18 16:28:17 +0200

    Add constructor to BitcoinExchange

--- ex00/include/BitcoinExchange.hpp
+++ ex00/include/BitcoinExchange.hpp
@@ -6,20 +6,20 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:42:25 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/06/08 05:25:17 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/06/18 16:26:09 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
-#include <string>
+#include "ADataFile.hpp"
 #include <map>
 
 class BitcoinExchange
 {
 public:
     ~BitcoinExchange();
+    BitcoinExchange(ADataFile const &bitcoinData, ADataFile const &userInput);
 
 private:
     BitcoinExchange();
--- ex00/source/BitcoinExchange.cpp
+++ ex00/source/BitcoinExchange.cpp
@@ -6,10 +6,14 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 14:08:08 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/06/07 18:13:59 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/06/18 16:27:46 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(ADataFile const &bitcoinData, ADataFile const &userInput)
+{
+}
