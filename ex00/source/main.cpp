0438dd326fb3624e6a9dabe45e0f28bbf8860772
SalahHW <salah.bouheniche@gmail.com>
2024-06-25 16:13:46 +0200

    Update tests

--- ex00/source/main.cpp
+++ ex00/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:41:43 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/06/18 16:40:27 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/06/25 00:26:46 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ int main(int argc, char **argv)
 		BitcoinData data(dataFilePath.c_str());
 		UserInput input(argv[1]);
 		BitcoinExchange(data, input);
+		Date test("2009-01-01");
+		// std::cout << data;
+		// data.getExchangeRate(test);
+		// std::cout << "=" << test << " = " << data.getExchangeRate(test) << std::endl;
 	}
 	catch (const std::exception &e)
 	{
