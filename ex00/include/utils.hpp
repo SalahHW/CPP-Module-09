43eac7703f4328d30ea8afe40d42aa9328dd488d
SalahHW <salah.bouheniche@gmail.com>
2024-06-26 19:58:43 +0200

    Add strtof in utils::

--- ex00/include/utils.hpp
+++ ex00/include/utils.hpp
@@ -1,15 +1,28 @@
-//
-// Created by salah on 22/06/24.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 13:26:34 by sbouheni          #+#    #+#             */
+/*   Updated: 2024/06/26 13:26:37 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <stdexcept>
 #include <string>
+#include <cerrno>
+#include <cstdlib>
+
 namespace utils
 {
 	bool isWhiteSpace(char c);
 	std::string trimWhitespace(const std::string& str);
+	float strtof(std::string const &str);
 }
 
 #endif //UTILS_HPP
--- ex00/source/utils.cpp
+++ ex00/source/utils.cpp
@@ -1,6 +1,14 @@
-//
-// Created by salah on 22/06/24.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 13:26:25 by sbouheni          #+#    #+#             */
+/*   Updated: 2024/06/26 14:39:10 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "utils.hpp"
 
@@ -20,4 +28,19 @@ std::string utils::trimWhitespace(const std::string& str)
 	}
 	std::string::size_type last = str.find_last_not_of(" \t\n\r\f\v");
 	return str.substr(first, (last - first + 1));
+}
+
+float utils::strtof(std::string const &str)
+{
+	errno = 0;
+	char *endptr = NULL;
+	if (str.empty())
+		throw std::runtime_error("Error while converting string to float");
+
+	float value = std::strtof(str.c_str(), &endptr);
+	if (errno == ERANGE)
+		throw std::runtime_error("Error while converting string to float");
+	if (*endptr != '\0')
+		throw std::runtime_error("Error while converting string to float");
+	return value;
 }
