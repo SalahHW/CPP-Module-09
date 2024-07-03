51b6c0bec6eeb8b5505127f62bc9f0cca6970e96
SalahHW <salah.bouheniche@gmail.com>
2024-07-03 15:10:42 +0200

    Add herror handling on bad argc

--- ex02/source/main.cpp
+++ ex02/source/main.cpp
@@ -6,13 +6,20 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 14:54:18 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/07/03 14:54:53 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/07/03 15:00:35 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./PmergeMe \"numbers separated by spaces\"" << std::endl;
+		return (1);
+	}
+	(void) argv;
 	return (0);
 }
