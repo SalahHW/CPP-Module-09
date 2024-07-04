6e5dd69638d23e5608415aa4e88df85ab9249614
SalahHW <salah.bouheniche@gmail.com>
2024-07-05 01:29:14 +0200

    Add fordJohnson sort aglortihm for list and vector

--- ex02/include/PmergeMe.hpp
+++ ex02/include/PmergeMe.hpp
@@ -6,19 +6,20 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 14:49:56 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/07/05 00:11:21 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/07/05 01:08:01 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <cstdlib>
-#include <iostream>
-#include <vector>
-#include <list>
-#include <string>
 #include <cerrno>
 #include <climits>
+#include <ctime>
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
 
 class PmergeMe
 {
@@ -31,6 +32,10 @@ public:
 
 	void printVector() const;
 	void printList() const;
+	void merge(std::vector<int>::iterator left, std::vector<int>::iterator middle, std::vector<int>::iterator right);
+	void merge(std::list<int>::iterator left, std::list<int>::iterator middle, std::list<int>::iterator right);
+	void fordJohnsonSort(std::vector<int> &container, std::vector<int>::iterator left, std::vector<int>::iterator right);
+	void fordJohnsonSort(std::list<int> &container, std::list<int>::iterator left, std::list<int>::iterator right);
 
 private:
 	PmergeMe();
